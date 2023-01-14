a82476370edbc1cec39fc7047ab6051c17ae9141
Kaan Arslan <kaanarslan35@gmail.com>
2023-01-14 11:26:14 +0300

    footer style changed

--- src/wrapper/AppWrap.js
+++ src/wrapper/AppWrap.js
@@ -9,7 +9,7 @@ const AppWrap = (Component, idName, classNames) => function HOC() {
         <Component />
 
         <div className="copyright">
-          <p className="p-text">@2022 Designed & Built by Kaan Arslan</p>
+          <p className="p-text">@2022 Built by Kaan Arslan</p>
          
         </div>
       </div>
